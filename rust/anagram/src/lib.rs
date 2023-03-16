574fa90d397d70fcf6e1b56fee693c06926b0a8c
kienngynh <kien.ngynh@example.com>
2023-03-16 08:27:34 +0700

    ko

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,7 +1,11 @@
 use std::collections::HashSet;
-
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{word}' word find anagrams among the following words: {possible_anagrams:?}"
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let mut set_anagrams: HashSet<&str> = HashSet::new();
+    let set_word:HashSet<char> = word.chars().map(|x|x.to_ascii_lowercase()).collect();
+    for s in possible_anagrams.iter() {
+        if s != &word && (set_word == s.chars().map(|x|x.to_ascii_lowercase()).collect()) {
+            set_anagrams.insert(s);
+        }
+    }
+    set_anagrams
 }
--- rust/anagram/src/main.rs
+++ rust/anagram/src/main.rs
@@ -1,7 +1,24 @@
 use std::collections::HashSet;
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let mut set_anagrams: HashSet<&str> = HashSet::new();
+    let set_word:HashSet<char> = word.chars().collect();
+    for s in possible_anagrams.iter() {
+        if s != &word && (set_word == s.chars().collect()) {
+            set_anagrams.insert(s);
+        }
+    }
+    set_anagrams
+}
 fn main() {
-    let mut book = HashSet::new();
-    book.insert("Hello".to_string());
-    book.insert("World".to_string());
-    println!("{:?}", book)
+    let word = "allergy";
+    let inputs = [
+        "gallery",
+        "ballerina",
+        "regally",
+        "clergy",
+        "largely",
+        "leading",
+    ];
+    let set = anagrams_for(word, &inputs);
+    println!("{:?}", set);
 }
