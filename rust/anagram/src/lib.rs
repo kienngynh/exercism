2d538a9707947870751cbef6b813787e330d15d0
kienngynh <kien.ngynh@example.com>
2023-03-16 23:10:30 +0700

    ok

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,15 @@
 use std::collections::HashSet;
+pub fn sort(word: &str) -> Vec<char> {
+    let mut vec_word:Vec<char> = word.to_lowercase().as_str().chars().collect();
+    vec_word.sort_unstable();
+    vec_word
+}
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
     let mut set_anagrams: HashSet<&str> = HashSet::new();
-    let set_word:HashSet<char> = word.chars().map(|x|x.to_ascii_lowercase()).collect();
+    let vec_word = sort(word);
     for s in possible_anagrams.iter() {
-        if s != &word && (set_word == s.chars().map(|x|x.to_ascii_lowercase()).collect()) {
+        let vec_s = sort(s);
+        if s.to_lowercase() != word.to_lowercase() && vec_s == vec_word {
             set_anagrams.insert(s);
         }
     }
--- rust/anagram/src/main.rs
+++ rust/anagram/src/main.rs
@@ -1,9 +1,14 @@
 use std::collections::HashSet;
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
     let mut set_anagrams: HashSet<&str> = HashSet::new();
-    let set_word:HashSet<char> = word.chars().collect();
+    let lowcase_word = word.to_lowercase();
+    let mut vec_word: Vec<u8> = lowcase_word.as_bytes().to_vec();
+    vec_word.sort();
     for s in possible_anagrams.iter() {
-        if s != &word && (set_word == s.chars().collect()) {
+        let lowcase_s = s.to_lowercase();
+        let mut vec_s: Vec<u8> = lowcase_s.as_bytes().to_vec();
+        vec_s.sort();
+        if lowcase_s != lowcase_word && vec_s == vec_word {
             set_anagrams.insert(s);
         }
     }
