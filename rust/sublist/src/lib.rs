d43309d2289ed402b3f469d3f41a01e7636021c0
kienngynh <kien.ngynh@example.com>
2023-03-20 21:12:33 +0700

    ok

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -6,21 +6,28 @@ pub enum Comparison {
     Unequal,
 }
 impl Comparison {
-    pub fn compare<T: PartialEq>(_short_list: &[T], _long_list: &[T]) -> Self {
-        Comparison::Sublist        
-    }
-    pub fn compare_equal<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Self {
-        if _first_list == _second_list {
-            Comparison::Equal
-        } else {
-            Comparison::Unequal
+    pub fn compare<T: PartialEq>(_short_list: &[T], _long_list: &[T]) -> bool {
+        let mut _long_list_cp = _long_list.clone();
+        for _i in _long_list.iter() {
+            if _long_list_cp.starts_with(_short_list) {
+                return true;
+            } else {
+                (_,_long_list_cp) = _long_list_cp.split_at(1);
+            }
         }
+        return false;
     }
 }
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    match _first_list.len() as i32 - _second_list.len() as i32 {
-        ..=-1 => Comparison::compare(_first_list, _second_list),
-        0 => Comparison::compare_equal(_first_list, _second_list),
-        1.. => Comparison::compare(_second_list, _first_list),
+    let v = match _first_list.len() as i32 - _second_list.len() as i32 {
+        ..=-1 => (Comparison::compare(_first_list, _second_list), -1),
+        0 => (_first_list == _second_list, 0),
+        1.. => (Comparison::compare(_second_list, _first_list), 1),
+    };
+    match v {
+        (true, -1) => Comparison::Sublist,
+        (true, 0) => Comparison::Equal,
+        (true, 1) => Comparison::Superlist,
+        _ => Comparison::Unequal,
     }
 }
--- rust/sublist/src/main.rs
+++ rust/sublist/src/main.rs
@@ -6,22 +6,25 @@ pub enum Comparison {
     Unequal,
 }
 impl Comparison {
-    pub fn compare<T: PartialEq>(_short_list: &[T], _long_list: &[T]) -> Self {
-        if _short_list == _long_list {
-            Comparison::Equal
-        } else {
-            Comparison::Unequal
-        }
+    pub fn compare<T: PartialEq>(_short_list: &[T], _long_list: &[T]) -> bool {
+        _short_list.iter().all(|x| _long_list.contains(x))
     }
 }
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    match _first_list.len() as i32 - _second_list.len() as i32 {
-        ..=0 => Comparison::compare(_first_list, _second_list),
-        1.. => Comparison::compare(_second_list, _first_list),
+    let v = match _first_list.len() as i32 - _second_list.len() as i32 {
+        ..=-1 => (Comparison::compare(_first_list, _second_list), -1),
+        0 => (_first_list == _second_list, 0),
+        1.. => (Comparison::compare(_second_list, _first_list), 1),
+    };
+    match v {
+        (true, -1) => Comparison::Sublist,
+        (true, 0) => Comparison::Equal,
+        (true, 1) => Comparison::Superlist,
+        _ => Comparison::Unequal,
     }
 }
 
 fn main() {
     let mut _first_list: Vec<u32> = (1..1_000_000).collect();
-    println!("{:?}", sublist(&[3, 4, 5], &[3, 4, 5]))
+    println!("{:?}", sublist(&_first_list, &[3, 4, 5]))
 }
