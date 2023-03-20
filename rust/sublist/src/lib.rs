ab3ce4ba99f24cb1f23077e54d19423267968324
kienngynh <kien.ngynh@example.com>
2023-03-20 08:40:11 +0700

    ok

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -5,7 +5,22 @@ pub enum Comparison {
     Superlist,
     Unequal,
 }
-
+impl Comparison {
+    pub fn compare<T: PartialEq>(_short_list: &[T], _long_list: &[T]) -> Self {
+        Comparison::Sublist        
+    }
+    pub fn compare_equal<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Self {
+        if _first_list == _second_list {
+            Comparison::Equal
+        } else {
+            Comparison::Unequal
+        }
+    }
+}
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    match _first_list.len() as i32 - _second_list.len() as i32 {
+        ..=-1 => Comparison::compare(_first_list, _second_list),
+        0 => Comparison::compare_equal(_first_list, _second_list),
+        1.. => Comparison::compare(_second_list, _first_list),
+    }
 }
--- rust/sublist/src/main.rs
+++ rust/sublist/src/main.rs
@@ -1,3 +1,27 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+impl Comparison {
+    pub fn compare<T: PartialEq>(_short_list: &[T], _long_list: &[T]) -> Self {
+        if _short_list == _long_list {
+            Comparison::Equal
+        } else {
+            Comparison::Unequal
+        }
+    }
+}
+pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+    match _first_list.len() as i32 - _second_list.len() as i32 {
+        ..=0 => Comparison::compare(_first_list, _second_list),
+        1.. => Comparison::compare(_second_list, _first_list),
+    }
+}
+
 fn main() {
-    println!("Hello World!")
+    let mut _first_list: Vec<u32> = (1..1_000_000).collect();
+    println!("{:?}", sublist(&[3, 4, 5], &[3, 4, 5]))
 }
