5e078cbc394b8e2d3fc1ec2158571c8d4e44f5a0
kienngynh <kien.ngynh@example.com>
2023-03-26 17:22:59 +0700

    ok

--- rust/minesweeper/src/main.rs
+++ rust/minesweeper/src/main.rs
@@ -10,50 +10,50 @@ fn remove_annotations_in_row(row: &str) -> String {
         })
         .collect()
 }
+pub fn get_index(position: (i32, i32), column: i32, row: i32) -> Option<usize> {
+    if position.0 >= 0 && position.0 < column && position.1 >= 0 && position.1 < row {
+        Some((position.0 * column + position.1) as usize)
+    } else {
+        None
+    }
+}
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
     let mut result: Vec<String> = Vec::new();
     let vector_minefield = minefield
         .iter()
         .flat_map(|&s| s.chars())
         .collect::<Vec<char>>();
-    let (x, y) = (minefield.len(), vector_minefield.len() / minefield.len());
+    let (colume, row) = (minefield.len(), vector_minefield.len() / minefield.len());
     let mut s = String::new();
     for (k, v) in vector_minefield.iter().enumerate() {
-        if k % x == 0 {
+        if k % colume == 0 {
             s = String::new()
         }
         let value = match (k, v) {
             (_, '*') => '*',
             (k, _) => {
+                let (p_x, p_y) = (k / colume, k % colume);
                 println!(
                     "{:?}",
                     (
-                        Some(vector_minefield[k - y - 1]),
-                        Some(vector_minefield[k - y]),
-                        Some(vector_minefield[k - y + 1]),
-                        Some(vector_minefield[k - 1]),
-                        Some(vector_minefield[k + 1]),
-                        Some(vector_minefield[k + y - 1]),
-                        Some(vector_minefield[k + y]),
-                        Some(vector_minefield[k + y + 1]),
+                        get_index((p_x as i32 - 1, p_y as i32 - 1), colume as i32, row as i32),
+                        get_index((p_x as i32 - 1, p_y as i32), colume as i32, row as i32),
+                        get_index((p_x as i32 - 1, p_y as i32 + 1), colume as i32, row as i32),
+                        get_index((p_x as i32, p_y as i32 - 1), colume as i32, row as i32),
+                        get_index((p_x as i32, p_y as i32 + 1), colume as i32, row as i32),
+                        get_index((p_x as i32 + 1, p_y as i32 - 1), colume as i32, row as i32),
+                        get_index((p_x as i32 + 1, p_y as i32), colume as i32, row as i32),
+                        get_index((p_x as i32 + 1, p_y as i32 + 1), colume as i32, row as i32),
                     )
                 );
-                '2'
+                ' '
             }
         };
         s.push_str(&value.to_string());
-        if k % x == x - 1 {
+        if k % colume == colume - 1 {
             result.push(s.clone())
         }
     }
-    /*
-    let (x, y) = (minefield.len(), vector_minefield.len());
-    for i in 0..y {
-        match (i / x, i % (y / x + 1)) {
-            (_, _) => todo!(),
-        }
-    }
-    */
     result
 }
 fn main() {
