1e16c856e187c5a85542c9540166c9eba254615f
kienngynh <kien.ngynh@example.com>
2023-03-29 23:03:37 +0700

    ok

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,4 +1,39 @@
-/// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {code} valid?");
+    let (mut e, mut o) = (vec![], vec![]);
+    let mut count_e: u32 = 0;
+    let mut count_o: u32 = 0;
+    let mut count: u32 = 0;
+    for c in code.chars() {
+        if c.is_digit(10) {
+            if count % 2 == 0 {
+                let d = c.to_digit(10).unwrap();
+                if d > 4 {
+                    count_e += 1
+                };
+                e.push(d);
+            } else {
+                let d = c.to_digit(10).unwrap();
+                if d > 4 {
+                    count_o += 1
+                };
+                o.push(d);
+            }
+            count += 1
+        } else if c == ' ' {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    let sum_e: u32 = e.iter().sum();
+    let sum_o: u32 = o.iter().sum();
+    let l = e.len() + o.len();
+    if l > 1 {
+        match l % 2 {
+            0 => (sum_e * 2 + sum_o - count_e * 9) % 10 == 0,
+            _ => (sum_o * 2 + sum_e - count_o * 9) % 10 == 0,
+        }
+    } else {
+        return false;
+    }
 }
--- rust/luhn/src/main.rs
+++ rust/luhn/src/main.rs
@@ -1,13 +1,43 @@
 pub fn is_valid(code: &str) -> bool {
-    let s = code
-        .chars()
-        .filter(|c| c.is_digit(10))
-        .map(|c| c.to_digit(10).unwrap())
-        .collect::<Vec<u32>>();
-    println!("{:?}", s);
-    true
+    let (mut e, mut o) = (vec![], vec![]);
+    let mut count_e: u32 = 0;
+    let mut count_o: u32 = 0;
+    let mut count: u32 = 0;
+    for c in code.chars() {
+        if c.is_digit(10) {
+            if count % 2 == 0 {
+                let d = c.to_digit(10).unwrap();
+                if d > 4 {
+                    count_e += 1
+                };
+                e.push(d);
+            } else {
+                let d = c.to_digit(10).unwrap();
+                if d > 4 {
+                    count_o += 1
+                };
+                o.push(d);
+            }
+            count += 1
+        } else if c == ' ' {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    let sum_e: u32 = e.iter().sum();
+    let sum_o: u32 = o.iter().sum();
+    let l = e.len() + o.len();
+    if l > 1 {
+        match l % 2 {
+            0 => (sum_e * 2 + sum_o - count_e * 9) % 10 == 0,
+            _ => (sum_o * 2 + sum_e - count_o * 9) % 10 == 0,
+        }
+    } else {
+        return false;
+    }
 }
 
 fn main() {
-    println!("{:?}", is_valid(" 8273 1232 7352 0569"))
+    println!("{:?}", is_valid("0593217891274"))
 }
