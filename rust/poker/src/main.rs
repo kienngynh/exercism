fefade58b59229693163ef7be5104241f7562dde
kienngynh <kien.ngynh@example.com>
2023-04-04 21:13:24 +0700

    ok

--- rust/poker/src/main.rs
+++ rust/poker/src/main.rs
@@ -1,3 +1,11 @@
+use std::collections::HashSet;
+fn hs_from<'a>(input: &'a [&'a str]) -> HashSet<&'a str> {
+    let mut hs = HashSet::new();
+    for item in input.iter() {
+        hs.insert(*item);
+    }
+    hs
+}
 fn main() {
-    
+    println!("{:?}", hs_from(&["2S 2H 2C 8D 2D", "4S 5H 5S 5D 5C"]));
 }
