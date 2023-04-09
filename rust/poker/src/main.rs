a28cc23471bbe290a72beb316a82896af23448e4
kienngynh <kien.ngynh@example.com>
2023-04-09 16:09:00 +0700

    ok

--- rust/poker/src/main.rs
+++ rust/poker/src/main.rs
@@ -7,5 +7,13 @@ fn hs_from<'a>(input: &'a [&'a str]) -> HashSet<&'a str> {
     hs
 }
 fn main() {
-    println!("{:?}", hs_from(&["2S 2H 2C 8D 2D", "4S 5H 5S 5D 5C"]));
+    let hs: HashSet<&str> = hs_from(&[
+        "4D 5S 6S 8D 3C",
+        "2S 4C 7S 9H 10H",
+        "3S 4S 5D 6H JH",
+        "3H 4H 5C 6C JD",
+    ]);
+    for i in hs {
+        println!("{:?}", i)
+    }
 }
