ef1894ff4a027cc49a1a2eb11706940dd08a7502
kienngynh <kien.ngynh@example.com>
2023-04-11 08:34:43 +0700

    quick update

--- rust/poker/src/main.rs
+++ rust/poker/src/main.rs
@@ -1,17 +1,13 @@
-use std::collections::HashSet;
-fn hs_from<'a>(input: &'a [&'a str]) -> HashSet<&'a str> {
-    let mut hs = HashSet::new();
-    for item in input.iter() {
-        hs.insert(*item);
-    }
-    hs
-}
+use poker::winning_hands;
+use std::collections::{HashMap, HashSet};
+
 fn main() {
     let input = &[
         "4D 5S 6S 8D 3C",
         "2S 4C 7S 9H 10H",
         "3S 4S 5D 6H JH",
         "3H 4H 5C 6C JD",
+        "2G 4Q 5A 6D KH",
     ];
-    println!("{:?}", input)
+    println!("{:?}", winning_hands(input))
 }
