56b98246ecaf0de56c71f1c7e66837434fda8ef8
kienngynh <kien.ngynh@example.com>
2023-04-23 15:14:17 +0700

    quick update

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -1,7 +1,6 @@
 /// Given a list of poker hands, return a list of those hands which win.
 ///
-/// Note the type signature: this function should return _the same_ reference to
-/// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
+/// Note the type signature: this function should return _the same_ reference to the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Clone, Copy, Hash)]
 enum Rank {
     Two,
