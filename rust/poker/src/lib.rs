68d4670ffce6d39d3acd3322b5f83e9fd781d32d
kienngynh <kien.ngynh@example.com>
2023-05-06 08:39:34 +0700

    ok

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -1,8 +1,17 @@
-use std::{cmp::Ordering, convert::TryFrom};
+use std::{ cmp::Ordering, convert::TryFrom, fmt::Result };
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
-#[derive(Debug, Clone, Copy)]
+struct Hand<'a> {
+    hand: &'a str,
+    cards: [Card; 5],
+    categories: Categories,
+}
+
+struct Card {
+    rank: Rank,
+    suit: Suit,
+}
 enum Rank {
     Two,
     Three,
@@ -18,47 +27,33 @@ enum Rank {
     King,
     Ace,
 }
-#[derive(Debug, Clone, Copy)]
 enum Suit {
     Spades,
     Clubs,
     Hearts,
     Diamonds,
 }
-#[derive(Debug, Clone, Copy)]
-struct Card {
-    rank: Rank,
-    suit: Suit,
-}
-#[derive(Debug)]
 enum Categories {
     HighCard,
     OnePair,
     TwoPair,
     ThreeOfAKind,
-    Straght,
+    Straight,
     Flush,
     FullHouse,
     FourOfAKind,
     StraightFlush,
 }
-#[derive(Debug)]
-struct Hand<'a> {
-    hand: &'a str,
-    cards: [Card; 5],
-    categories: Categories,
-}
 impl<'a> TryFrom<&'a str> for Hand<'a> {
     type Error = &'static str;
-
-    fn try_from(hand: &'a str) -> Result<Self, Self::Error> {
-        let cards = hand
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        let cards: Vec<_> = value
             .split_whitespace()
-            .map(|card|Card::try_from(card))
-            .collect::<Result<Vec<_>, _>>()?;
+            .map(|card| Card::try_from(card))
+            .collect();
         if cards.len() == 5 {
             Ok(Hand {
-                hand: hand,
+                hand: value,
                 cards: [cards[0], cards[1], cards[2], cards[3], cards[4]],
                 categories: Categories::HighCard,
             })
@@ -69,56 +64,26 @@ impl<'a> TryFrom<&'a str> for Hand<'a> {
 }
 impl<'a> TryFrom<&'a str> for Card {
     type Error = &'static str;
-    fn try_from(card: &'a str) -> Result<Self, Self::Error> {
-        match card.len() {
-            3 => Ok(Card {
-                rank: Rank::try_from(&card[..2])?, // add ? into end because try_from for rank return Result but rank is enum Rank
-                suit: Suit::try_from(&card[2..])?,
-            }),
-            2 => Ok(Card {
-                rank: Rank::try_from(&card[..1])?,
-                suit: Suit::try_from(&card[1..])?,
-            }),
-            _ => Err("Invalid card"),
-        }
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        todo!()
     }
 }
-
 impl<'a> TryFrom<&'a str> for Rank {
     type Error = &'static str;
-    fn try_from(rank: &'a str) -> Result<Self, Self::Error> {
-        match rank {
-            "2" => Ok(Rank::Two),
-            "3" => Ok(Rank::Three),
-            "4" => Ok(Rank::Four),
-            "5" => Ok(Rank::Five),
-            "6" => Ok(Rank::Six),
-            "7" => Ok(Rank::Seven),
-            "8" => Ok(Rank::Eight),
-            "9" => Ok(Rank::Nine),
-            "10" => Ok(Rank::Ten),
-            "J" => Ok(Rank::Jack),
-            "K" => Ok(Rank::Queen),
-            "Q" => Ok(Rank::King),
-            "A" => Ok(Rank::Ace),
-            _ => Err("Invalid rank"),
-        }
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        todo!()
     }
 }
 impl<'a> TryFrom<&'a str> for Suit {
     type Error = &'static str;
-    fn try_from(suit: &'a str) -> Result<Self, Self::Error> {
-        match suit {
-            "S" => Ok(Suit::Spades),
-            "C" => Ok(Suit::Clubs),
-            "H" => Ok(Suit::Hearts),
-            "D" => Ok(Suit::Diamonds),
-            _ => Err("Invalid suit"),
-        }
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        todo!()
     }
 }
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
-    let hand: Vec<_> = hands.iter().map(|hand| Hand::try_from(*hand)).collect();
-    println!("{:?}", hand);
+    let hand: Vec<_> = hands
+        .iter()
+        .map(|hand| Hand::try_from(*hand))
+        .collect();
     vec!["4D 3D 2H 2S AC"]
 }
