e40a660121d2a5590c323b3a6b5d86eca943dd52
kienngynh <kien.ngynh@example.com>
2023-05-06 08:54:37 +0700

    ok

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -2,16 +2,19 @@ use std::{ cmp::Ordering, convert::TryFrom, fmt::Result };
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
+#[derive(Debug)]
 struct Hand<'a> {
     hand: &'a str,
     cards: [Card; 5],
     categories: Categories,
 }
 
+#[derive(Debug)]
 struct Card {
     rank: Rank,
     suit: Suit,
 }
+#[derive(Debug)]
 enum Rank {
     Two,
     Three,
@@ -27,12 +30,16 @@ enum Rank {
     King,
     Ace,
 }
+
+#[derive(Debug)]
 enum Suit {
     Spades,
     Clubs,
     Hearts,
     Diamonds,
 }
+
+#[derive(Debug)]
 enum Categories {
     HighCard,
     OnePair,
@@ -47,10 +54,10 @@ enum Categories {
 impl<'a> TryFrom<&'a str> for Hand<'a> {
     type Error = &'static str;
     fn try_from(value: &'a str) -> Result<Self, Self::Error> {
-        let cards: Vec<_> = value
+        let cards = value
             .split_whitespace()
             .map(|card| Card::try_from(card))
-            .collect();
+            .collect::<Result<Vec<_>, _>>()?;
         if cards.len() == 5 {
             Ok(Hand {
                 hand: value,
@@ -65,19 +72,46 @@ impl<'a> TryFrom<&'a str> for Hand<'a> {
 impl<'a> TryFrom<&'a str> for Card {
     type Error = &'static str;
     fn try_from(value: &'a str) -> Result<Self, Self::Error> {
-        todo!()
+        match value.len() {
+            3 =>
+                Ok(Card { rank: Rank::try_from(&value[..2])?, suit: Suit::try_from(&value[2..])? }),
+            2 =>
+                Ok(Card { rank: Rank::try_from(&value[..1])?, suit: Suit::try_from(&value[1..])? }),
+            _ => Err("Invalid card"),
+        }
     }
 }
 impl<'a> TryFrom<&'a str> for Rank {
     type Error = &'static str;
     fn try_from(value: &'a str) -> Result<Self, Self::Error> {
-        todo!()
+        match value {
+            "2" => Ok(Rank::Two),
+            "3" => Ok(Rank::Three),
+            "4" => Ok(Rank::Four),
+            "5" => Ok(Rank::Five),
+            "6" => Ok(Rank::Six),
+            "7" => Ok(Rank::Seven),
+            "8" => Ok(Rank::Eight),
+            "9" => Ok(Rank::Nine),
+            "10" => Ok(Rank::Ten),
+            "J" => Ok(Rank::Jack),
+            "Q" => Ok(Rank::Queen),
+            "K" => Ok(Rank::King),
+            "A" => Ok(Rank::Ace),
+            _ => Err("Invalid rank"),
+        }
     }
 }
 impl<'a> TryFrom<&'a str> for Suit {
     type Error = &'static str;
     fn try_from(value: &'a str) -> Result<Self, Self::Error> {
-        todo!()
+        match value {
+            "S" => Ok(Suit::Spades),
+            "C" => Ok(Suit::Clubs),
+            "H" => Ok(Suit::Hearts),
+            "D" => Ok(Suit::Diamonds),
+            _ => Err("Invalid suit"),
+        }
     }
 }
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
