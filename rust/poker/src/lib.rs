9c84d761f867a6dd31702a1a218edce6f923bc1a
kienngynh <kien.ngynh@example.com>
2023-05-06 13:35:13 +0700

    ok

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{ cmp::Ordering, convert::TryFrom, fmt::Result };
+use std::{ cmp::Ordering, convert::TryFrom };
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
@@ -8,13 +8,17 @@ struct Hand<'a> {
     cards: [Card; 5],
     categories: Categories,
 }
-
-#[derive(Debug)]
+impl<'a> Hand<'a> {
+    fn check_categories(&self) -> Categories {
+        todo!()
+    }
+}
+#[derive(Debug, Clone, Copy)]
 struct Card {
     rank: Rank,
     suit: Suit,
 }
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Rank {
     Two,
     Three,
@@ -31,7 +35,7 @@ enum Rank {
     Ace,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Suit {
     Spades,
     Clubs,
@@ -57,7 +61,7 @@ impl<'a> TryFrom<&'a str> for Hand<'a> {
         let cards = value
             .split_whitespace()
             .map(|card| Card::try_from(card))
-            .collect::<Result<Vec<_>, _>>()?;
+            .collect::<Result<Vec<Card>, &'static str>>()?;
         if cards.len() == 5 {
             Ok(Hand {
                 hand: value,
@@ -119,5 +123,6 @@ pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
         .iter()
         .map(|hand| Hand::try_from(*hand))
         .collect();
+    println!("{:?}", hand);
     vec!["4D 3D 2H 2S AC"]
 }
